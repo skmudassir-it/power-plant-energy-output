dee8fe55a1bd8ca24e8a2643bafc80c6afb4c920
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2022-06-21 18:16:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, render_template, request, url_for
+import joblib
+import numpy as np
+app = Flask(__name__)
+solve = joblib.load('model')
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    eo = ''
+    if request.method == 'POST' and 'temperature' in request.form and 'vacuum' in request.form and 'pressure' in request.form and 'humidity' in request.form:
+        Temperature = float(request.form.get('temperature'))
+        Vacuum = float(request.form.get('vacuum'))
+        Pressure = float(request.form.get('pressure'))
+        Humidity = float(request.form.get('humidity'))
+        m = [[Temperature,Vacuum,Pressure,Humidity]]
+        eo = round(solve.predict(m),2)
+    return render_template('index.html',eo = eo)
+
+if __name__ == '__main__':
+    app.run(debug=True)
